27a15719c46fa24ff4ad6b78184bd9a0cee036e7
Antoine <antoinedujardin42@gmail.com>
2019-10-10 16:44:12 +0200

    add getBookings

--- src/Service/Api/bookingApi.js
+++ src/Service/Api/bookingApi.js
@@ -1,6 +1,17 @@
 const JetPack = require('../../Entity/Jetpack');
+const Booking = require('../../Entity/booking');
+
 module.exports = class  {
     constructor(httpClient) {
         this.httpClient = httpClient;
+    }
+
+    getBookings() {
+        return this.httpClient.fetch('/bookings', {}).then(rows => {
+
+            return rows.map(row => {
+                return new Booking(row["jetpack"], row["start_date"], row["end_date"]);
+            });
+        });
     }
 };
--- src/Service/Api/bookingApi.test.js
+++ src/Service/Api/bookingApi.test.js
@@ -1,2 +1,27 @@
-const BookingApi = require('./bookingApi.js');
+const BookingApi = require('./bookingApi');
 const JetPack = require('../../Entity/Jetpack');
+const Booking = require('../../Entity/booking');
+
+describe('BookingApi get Bookings', function () {
+
+    test('Test getBookings', () => {
+        let httpClientMock = {
+            fetch: jest.fn()
+        };
+
+        httpClientMock.fetch.mockResolvedValue([
+            {
+                jetpack: "a26574f0-3dd0-4e3b-9c1d-6089619f2f80",
+                start_date: "2019-01-01",
+                end_date: "2042-01-01"
+            }
+        ]);
+
+        let bookingApi = new BookingApi(httpClientMock);
+        bookingApi.getBookings().then(resp => {
+            expect(Array.isArray(resp)).toBe(true);
+            expect(resp.length).toBe(1);
+            expect(resp[0]).toBeInstanceOf(Booking)
+        });
+    });
+});
