73efd5ec5b9d81e6aaf7311bb4edf33f8a85a52c
a2lino <araujo_adelino@orange.fr>
2019-10-27 19:07:50 +0100

    try fixing stryker > travis error

--- stryker.conf.js
+++ stryker.conf.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
   config.set({
     mutator: "javascript",
     packageManager: "npm",
-    reporters: ["html", "baseline", "clear-text", "progress"],
+    reporters: ["html", "clear-text", "progress"],
     testRunner: "jest",
     transpilers: [],
     coverageAnalysis: "off"
