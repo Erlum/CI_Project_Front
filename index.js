2756e5300e77805ebda81a3389b37108a84a7a7e
Antoine <antoinedujardin42@gmail.com>
2019-11-01 16:57:41 +0100

    what the fuck

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const appConfig = require('./app.config');
 const JetpackService = require('./src/Service/Api/JetpackApi');
+const BookingService = require('./src/Service/Api/BookingApi');
 const HttpClient = require('./src/HttpClient');
 
 const httpClient = new HttpClient(appConfig.apiUrl);
 const jetpackService = new JetpackService(httpClient);
+const bookingService = new BookingService(httpClient);
 
 
 jetpackService.getJetPacks().then(jetpacks => {
@@ -15,7 +17,7 @@ jetpackService.getJetPacks().then(jetpacks => {
             '  <img src="'+ jetpack.image +'" class="card-img-top" alt="...">\n' +
             '  <div class="card-body">\n' +
             '    <h4 class="card-title">' + jetpack.name + '</h4>\n' +
-            '    <span id="jetpack-id" class="invisible">' + jetpack.id + '</span>' +
+            '    <span id="jetpack-id" class="hidden">' + jetpack.id + '</span>' +
             '     <div class="d-flex justify-content-around">' +
             '         <button type="button" id="modifier-jetpack" class="btn btn-outline-primary m" data-toggle="modal" data-target="#modifierModal"style="">Modifier</button>' +
             '         <button type="button" id="reserver-jetpack"class="btn btn-outline-success" data-toggle="modal" data-target="#reserverModal">Réserver</button>' +
