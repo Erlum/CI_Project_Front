560cb280b75115138827de93a3ea575e9c0cffbe
Antoine <antoinedujardin42@gmail.com>
2019-11-02 00:49:41 +0100

    style update

--- index.js
+++ index.js
@@ -10,21 +10,19 @@ const jetpackService = new JetpackService(httpClient);
 const bookingService = new BookingService(httpClient);
 
 let make_jetpack_block = function (jetpack) {
-    return '<div class="col-lg-4 col-md-6 mb-4">' +
-        '<div class="card h-100" id="jetpack-' + jetpack.id + '" style="width: 18rem;">\n' +
-        '  <img src="' + jetpack.image + '" class="card-img-top" alt="...">\n' +
-        '  <div class="card-body">\n' +
-        '    <h4 class="card-title">' + jetpack.name + '</h4>\n' +
-        '     <div class="d-flex justify-content-around">' +
-        '         <button type="button" id="edit-jetpack" class="btn btn-outline-primary m" data-toggle="modal" data-target="#edit-jetpack-modal" data-id="' + jetpack.id + '">Modifier</button>' +
-        '         <button type="button" id="book-jetpack" class="btn btn-outline-success" data-toggle="modal" data-target="#book-jetpack-modal" data-id="' + jetpack.id + '">Réserver</button>' +
-        '    </div>' +
-        '    <div class="text-center">' +
-        '           <button type="button" id="delete-jetpack" class="btn btn-outline-danger mt-2" data-toggle="modal" data-target="#delete-jetpack-modal" data-id="' + jetpack.id + '">Supprimer</button>' +
-        '     </div>' +
-        '  </div>\n' +
-        '</div>' +
-        '</div>';
+    return '<div class="col-lg-4 col-md-6 mb-4">\n' +
+        '    <div class="card h-100" id="jetpack-' + jetpack.id + '">\n' +
+        '        <img src="' + jetpack.image + '" class="card-img-top" alt="...">\n' +
+        '        <div class="card-body">\n' +
+        '            <h4 class="card-title">' + jetpack.name + '</h4>\n' +
+        '            <div class="d-flex justify-content-between flex-wrap">\n' +
+        '                <button type="button" id="edit-jetpack" class="btn btn-outline-primary m" data-toggle="modal" data-target="#edit-jetpack-modal" data-id="' + jetpack.id + '">Modifier</button>\n' +
+        '                <button type="button" id="book-jetpack" class="btn btn-outline-success" data-toggle="modal" data-target="#book-jetpack-modal" data-id="' + jetpack.id + '">Réserver</button>\n' +
+        '                <button type="button" id="delete-jetpack" class="btn btn-outline-danger" data-toggle="modal" data-target="#delete-jetpack-modal" data-id="' + jetpack.id + '">Supprimer</button>\n' +
+        '            </div>\n' +
+        '        </div>\n' +
+        '    </div>\n' +
+        '</div>\n';
 };
 
 // Load data
