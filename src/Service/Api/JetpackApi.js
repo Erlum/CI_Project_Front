26b2b4219845ea8dacec74b43870fd407672241c
a2lino <araujo_adelino@orange.fr>
2019-11-01 21:02:47 +0100

    add button working

--- src/Service/Api/JetpackApi.js
+++ src/Service/Api/JetpackApi.js
@@ -38,7 +38,8 @@ module.exports = class {
             image: jetPack.image,
             method: "post"
         }).then(response => {
-            jetPack.id = response[0].id;
+            jetPack.id = response[0].id
+            console.log(jetPack.id)
         });
     }
 
@@ -58,3 +59,4 @@ module.exports = class {
         });
     }
 };
+
