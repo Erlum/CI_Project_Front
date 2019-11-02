41133b236b8568ad5b6f9425edefecefbb94c3bb
Antoine <antoinedujardin42@gmail.com>
2019-11-02 17:53:21 +0100

    alert user if server connection is impossible

--- index.js
+++ index.js
@@ -8,32 +8,38 @@ const httpClient = new HttpClient(appConfig.apiUrl);
 const jetpackService = new JetpackService(httpClient);
 const bookingService = new BookingService(httpClient);
 
+let make_jetpack_block = function (jetpack) {
+    return '<div class="col-lg-4 col-md-6 mb-4">' +
+        '<div class="card h-100" id="jetpack-' + jetpack.id + '" style="width: 18rem;">\n' +
+        '  <img src="' + jetpack.image + '" class="card-img-top" alt="...">\n' +
+        '  <div class="card-body">\n' +
+        '    <h4 class="card-title">' + jetpack.name + '</h4>\n' +
+        '     <div class="d-flex justify-content-around">' +
+        '         <button type="button" id="edit-jetpack" class="btn btn-outline-primary m" data-toggle="modal" data-target="#edit-jetpack-modal" data-id="' + jetpack.id + '">Modifier</button>' +
+        '         <button type="button" id="book-jetpack" class="btn btn-outline-success" data-toggle="modal" data-target="#book-jetpack-modal" data-id="' + jetpack.id + '">Réserver</button>' +
+        '    </div>' +
+        '    <div class="text-center">' +
+        '           <button type="button" id="delete-jetpack" class="btn btn-outline-danger mt-2" data-toggle="modal" data-target="#delete-jetpack-modal" data-id="' + jetpack.id + '">Supprimer</button>' +
+        '     </div>' +
+        '  </div>\n' +
+        '</div>' +
+        '</div>';
+};
+
 // Load data
 let jetpacks_array = {};
 jetpackService.getJetPacks().then(jetpacks => {
     let html =  '';
     jetpacks.forEach((jetpack) => {
         jetpacks_array[jetpack.id] = jetpack;
-        html +=
-            '<div class="col-lg-4 col-md-6 mb-4">' +
-            '<div class="card h-100" id="jetpack-' + jetpack.id + '" style="width: 18rem;">\n' +
-            '  <img src="'+ jetpack.image +'" class="card-img-top" alt="...">\n' +
-            '  <div class="card-body">\n' +
-            '    <h4 class="card-title">' + jetpack.name + '</h4>\n' +
-            '     <div class="d-flex justify-content-around">' +
-            '         <button type="button" id="edit-jetpack" class="btn btn-outline-primary m" data-toggle="modal" data-target="#edit-jetpack-modal" data-id="' + jetpack.id + '">Modifier</button>' +
-            '         <button type="button" id="book-jetpack" class="btn btn-outline-success" data-toggle="modal" data-target="#book-jetpack-modal" data-id="' + jetpack.id + '">Réserver</button>' +
-            '    </div>' +
-            '    <div class="text-center">' +
-            '           <button type="button" id="delete-jetpack" class="btn btn-outline-danger mt-2" data-toggle="modal" data-target="#delete-jetpack-modal" data-id="' + jetpack.id + '">Supprimer</button>' +
-            '     </div>' +
-            '  </div>\n' +
-            '</div>' +
-            '</div>'
-
+        html += make_jetpack_block(jetpack);
     });
 
     document.getElementById('jetpacks').innerHTML = html;
+}).catch(function (error) {
+    if (error instanceof TypeError && error.message === "Failed to fetch"){
+        alert("Erreur: connection au serveur impossible")
+    }
 });
 
 // DOM Ready
