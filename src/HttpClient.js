eb088784b5d5926eca5df2f5ce4666d859727788
Antoine <antoinedujardin42@gmail.com>
2019-11-02 17:05:06 +0100

    throw error on 4xx/5xx

--- src/HttpClient.js
+++ src/HttpClient.js
@@ -3,7 +3,14 @@ module.exports = class  {
         this.url = url;
     }
 
-    fetch (path, options) {
-        return fetch(this.url + path, options).then(response => response.json());
+    fetch(path, options) {
+        return fetch(this.url + path, options)
+            .then(function (response) {
+                    if (!response.ok) {
+                        throw new Error('Bad status code from server: ' + response.status + response.statusText);
+                    }
+                    return response.bodyUsed ? response.json() : {};
+                }
+            );
     }
 };
