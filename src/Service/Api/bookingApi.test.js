ad19cce51a01ce3faa182cb182960e39380b80bc
Antoine <antoinedujardin42@gmail.com>
2019-11-03 17:00:47 +0100

    fix booking api get by jetpack test

--- src/Service/Api/bookingApi.test.js
+++ src/Service/Api/bookingApi.test.js
@@ -35,26 +35,38 @@ describe('BookingApi get Bookings', function () {
 });
 
 describe('BookingApi get JetPack Bookings', function () {
+    let httpClientMock = {
+        fetch: jest.fn()
+    };
 
-    test('Test getBookings', () => {
-        let httpClientMock = {
-            fetch: jest.fn()
-        };
+    let jetpack_id = "a26574f0-3dd0-4e3b-9c1d-6089619f2f80";
+    httpClientMock.fetch.mockResolvedValue([
+        {
+            idjetpack: jetpack_id,
+            startdate: "2019-01-01",
+            enddate: "2042-01-01"
+        }
+    ]);
 
-        let jetpack_id = "a26574f0-3dd0-4e3b-9c1d-6089619f2f80";
-        httpClientMock.fetch.mockResolvedValue([
-            {
-                idjetpack: jetpack_id,
-                startdate: "2019-01-01",
-                enddate: "2042-01-01"
-            }
-        ]);
+    let bookingApi = new BookingApi(httpClientMock);
 
-        let bookingApi = new BookingApi(httpClientMock);
-        bookingApi.getJetPackBookings(new JetPack("", "", jetpack_id)).then(resp => {
+    test('Test getJetPackBookings return type', () => {
+        return bookingApi.getJetPackBookings(new JetPack("", "", jetpack_id)).then(resp => {
             expect(Array.isArray(resp)).toBe(true);
+        });
+    });
+    test('Test getJetPackBookings return length', () => {
+        return bookingApi.getJetPackBookings(new JetPack("", "", jetpack_id)).then(resp => {
             expect(resp.length).toBe(1);
+        });
+    });
+    test('Test getJetPackBookings return unit type', () => {
+        return bookingApi.getJetPackBookings(new JetPack("", "", jetpack_id)).then(resp => {
             expect(resp[0]).toBeInstanceOf(Booking);
+        });
+    });
+    test('Test getJetPackBookings return unit jetpack id', () => {
+        return bookingApi.getJetPackBookings(new JetPack("", "", jetpack_id)).then(resp => {
             expect(resp[0].jetPack).toBe(jetpack_id);
         });
     });
