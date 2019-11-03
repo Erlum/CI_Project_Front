46c46926c3ce2ab407a02752132ba5bfc8053c97
Antoine <antoinedujardin42@gmail.com>
2019-11-03 16:57:01 +0100

    improve booking test

--- src/Entity/booking.test.js
+++ src/Entity/booking.test.js
@@ -1,17 +1,19 @@
 const Booking = require('./Booking');
 
 describe('Booking toJson', function () {
+    let booking = new Booking("987", "12-12-2019", "25-12-2019", "123");
+    let booking_no_booking_id = new Booking("987", "12-12-2019", "25-12-2019");
 
-    test('Test toJson', () => {
-        let booking = new Booking("987", "12-12-2019", "25-12-2019", "123");
-        let booking_empty_fields = new Booking("987", "12-12-2019", "25-12-2019");
+    test('Test toJson all information', () => {
         expect(booking.toJson()).toMatchObject({
             id: "123",
             start: "12-12-2019",
             end: "25-12-2019",
             jetPack: "987"
         });
-        expect(booking_empty_fields.toJson()).toMatchObject({
+    });
+    test('Test toJson empty booking id', () => {
+        expect(booking_no_booking_id.toJson()).toMatchObject({
             id: null,
             start: "12-12-2019",
             end: "25-12-2019",
