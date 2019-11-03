12fac1d8eca33e83394c9cd0844f2f3cbfcabcc7
Antoine <antoinedujardin42@gmail.com>
2019-11-03 01:01:33 +0100

    fix edit jetpack api test

--- src/Entity/booking.test.js
+++ src/Entity/booking.test.js
@@ -4,7 +4,7 @@ describe('Booking toJson', function () {
 
     test('Test toJson', () => {
         let booking = new Booking("987", "12-12-2019", "25-12-2019", "123");
-        let booking_no_id = new Booking("987", "12-12-2019", "25-12-2019");
+        let booking_empty_fields = new Booking("987", "12-12-2019", "25-12-2019");
         expect(booking.toJson()).toMatchObject({
             id: "123",
             start: "12-12-2019",
