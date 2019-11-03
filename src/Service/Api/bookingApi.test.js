e90d0b0c2422971184838f824812f49d15fcb2f0
Antoine <antoinedujardin42@gmail.com>
2019-11-03 16:58:51 +0100

    fix booking api get all test

--- src/Service/Api/bookingApi.test.js
+++ src/Service/Api/bookingApi.test.js
@@ -3,24 +3,32 @@ const JetPack = require('../../Entity/Jetpack');
 const Booking = require('../../Entity/Booking');
 
 describe('BookingApi get Bookings', function () {
+    let httpClientMock = {
+        fetch: jest.fn()
+    };
 
-    test('Test getBookings', () => {
-        let httpClientMock = {
-            fetch: jest.fn()
-        };
+    httpClientMock.fetch.mockResolvedValue([
+        {
+            idjetpack: "a26574f0-3dd0-4e3b-9c1d-6089619f2f80",
+            startdate: "2019-01-01",
+            enddate: "2042-01-01"
+        }
+    ]);
 
-        httpClientMock.fetch.mockResolvedValue([
-            {
-                idjetpack: "a26574f0-3dd0-4e3b-9c1d-6089619f2f80",
-                startdate: "2019-01-01",
-                enddate: "2042-01-01"
-            }
-        ]);
+    let bookingApi = new BookingApi(httpClientMock);
 
-        let bookingApi = new BookingApi(httpClientMock);
-        bookingApi.getBookings().then(resp => {
+    test('Test getBookings return type', () => {
+        return bookingApi.getBookings().then(resp => {
             expect(Array.isArray(resp)).toBe(true);
+        });
+    });
+    test('Test getBookings return length', () => {
+        return bookingApi.getBookings().then(resp => {
             expect(resp.length).toBe(1);
+        });
+    });
+    test('Test getBookings return unit type', () => {
+        return bookingApi.getBookings().then(resp => {
             expect(resp[0]).toBeInstanceOf(Booking);
         });
     });
