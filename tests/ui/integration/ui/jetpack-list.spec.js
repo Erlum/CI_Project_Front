a313b5f5e128c0004cd2d6663f42ec8ae40973b8
a2lino <araujo_adelino@orange.fr>
2019-11-09 15:30:48 +0100

    remove cypress test mock data

--- tests/ui/integration/ui/jetpack-list.spec.js
+++ tests/ui/integration/ui/jetpack-list.spec.js
@@ -1,13 +1,4 @@
 /// <reference types="Cypress" />
 
-context('Jetpack list', () => {
-    beforeEach(() => {
-        cy.visit('/')
-    })
 
-    it('List all Jetpacks', () => {
-        cy.contains('Jetpack Fortnite Wiki\n')
-    })
-
-});
 
