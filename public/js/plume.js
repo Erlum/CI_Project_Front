e0c9c0f1cfb3b1594a61cb97dab99195f697b261
Antoine <antoinedujardin42@gmail.com>
2019-11-10 17:09:17 +0100

    [WIP] particles collisions

--- public/js/plume.js
+++ public/js/plume.js
@@ -20,13 +20,31 @@ const MAX_SPEED_MODIFIER = 14;
 const PARTICLE_SIZE = 3;
 const PARTICLE_PER_UPDATE = 15;
 
+const tightness = 20;
+/**
+ * Returns a random number between 0 and 1 with normal distribution
+ * source: https://stackoverflow.com/questions/25582882/javascript-math-random-normal-distribution-gaussian-bell-curve/36481059#36481059
+ * @returns {number}
+ */
+function gaussianRand() {
+    var rand = 0;
+
+    for (var i = 0; i < tightness; i += 1) {
+        rand += Math.random();
+    }
+
+    return rand / tightness;
+}
+
+
 //The class we will use to store particles. It includes x and y
 //coordinates, horizontal and vertical PARTICLE_BASE_SPEED, and how long it's
 //been "alive" for.
-function Particle(x, y, speed) {
+function Particle(x, y, speed, direction) {
     this.x = x;
     this.y = y;
     this.speed = speed;
+    this.direction = direction;
     this.lifespan = Math.random() * 5;
 }
 
@@ -97,21 +115,25 @@ function update() {
         let startY = lastY + CURSOR_OFFSET;
         let endX = mouseX + CURSOR_OFFSET;
         let endY = mouseY + CURSOR_OFFSET;
-        for (let i = 0; i < PARTICLE_PER_UPDATE; i++) {
-            let position_fraction = (PARTICLE_PER_UPDATE - i) / PARTICLE_PER_UPDATE;
-            let inverse_position_fraction = 1 - position_fraction;
-            //Adds a particle at the mouse position, with random horizontal and vertical speeds
-            if (mouseDown && speed_modifier < MAX_SPEED_MODIFIER){
-                speed_modifier = speed_modifier + (0.005 * (MAX_SPEED_MODIFIER - speed_modifier) / MAX_SPEED_MODIFIER)
+        // Don't create particles out of screen.
+        if ((startX < width || endX < width) && (startY < height || endY < height)){
+            for (let i = 0; i < PARTICLE_PER_UPDATE; i++) {
+                let position_fraction = (PARTICLE_PER_UPDATE - i) / PARTICLE_PER_UPDATE;
+                let inverse_position_fraction = 1 - position_fraction;
+                //Adds a particle at the mouse position, with random horizontal and vertical speeds
+                if (mouseDown && speed_modifier < MAX_SPEED_MODIFIER){
+                    speed_modifier = speed_modifier + (0.005 * (MAX_SPEED_MODIFIER - speed_modifier) / MAX_SPEED_MODIFIER)
+                }
+                else if ((! mouseDown) && speed_modifier > 1) {
+                    speed_modifier = speed_modifier - (0.005 * (1 - (MAX_SPEED_MODIFIER - speed_modifier) / MAX_SPEED_MODIFIER) + 0.0001)
+                }
+                let p = new Particle(
+                    startX * position_fraction + endX * inverse_position_fraction + i % 4,
+                    startY * position_fraction + endY * inverse_position_fraction + i % 4,
+                    Math.random() * 2 + PARTICLE_BASE_SPEED * speed_modifier,
+                    1/4);
+                particles.push(p);
             }
-            else if ((! mouseDown) && speed_modifier > 1) {
-                speed_modifier = speed_modifier - (0.005 * (1 - (MAX_SPEED_MODIFIER - speed_modifier) / MAX_SPEED_MODIFIER) + 0.0001)
-            }
-            let p = new Particle(
-                startX * position_fraction + endX * inverse_position_fraction + i % 4,
-                startY * position_fraction + endY * inverse_position_fraction + i % 4,
-                Math.random() * 2 + PARTICLE_BASE_SPEED * speed_modifier);
-            particles.push(p);
         }
     }
 
@@ -142,8 +164,25 @@ function update() {
         //Move the particle based on its horizontal and vertical speeds
         let trail_speed = particles[i].speed * (0.4 + lifespan_fraction);
         let random_orthogonal_modifier = ((1 - lifespan_fraction) * 1.5 ** 1.5) * (Math.random() * (2) - 1) * 3;
-        particles[i].x += trail_speed + random_orthogonal_modifier;
-        particles[i].y += trail_speed - random_orthogonal_modifier;
+        particles[i].x += trail_speed * Math.cos(particles[i].direction * Math.PI) + random_orthogonal_modifier;
+        particles[i].y += trail_speed * Math.sin(particles[i].direction * Math.PI) - random_orthogonal_modifier;
+
+        // Check if the particle has collided with a wall
+        if (particles[i].x > width || particles[i].y > height){
+            let wall_direction = particles[i].x > width ? 1/2 : -2;
+            let max_direction = particles[i].direction + 1/2;
+            while (particles[i].direction < 0){
+                particles[i].direction += 2;
+                wall_direction += 2;
+                max_direction += 2;
+            }
+            let new_direction = gaussianRand() * (max_direction - wall_direction) + wall_direction;
+            particles[i].speed *= (max_direction - particles[i].direction) / max_direction;
+            if((max_direction - particles[i].direction) / max_direction > 10){
+                console.log((max_direction - particles[i].direction) / max_direction)
+            }
+            particles[i].direction = new_direction;
+        }
 
         particles[i].lifespan++;
         //If the particle has lived longer than we are allowing, remove it from the array.
