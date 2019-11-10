bc6575500a383e509a706ea8a99928ed5cf7fcb4
Antoine <antoinedujardin42@gmail.com>
2019-11-10 17:27:37 +0100

    improve toCard function

--- src/Entity/Jetpack.js
+++ src/Entity/Jetpack.js
@@ -45,40 +45,22 @@ module.exports = class  {
         }
     }
 
-    toCardAllJetPacks(){
+    toCard(show_book_button=false){
         return '' +
         '<div class="col-lg-4 col-md-6 mb-4">' +
-        '    <div class="card h box zoom" style="width: 18rem;">\n' +
+        '    <div class="card h box zoom">\n' +
         '        <img src="'+ this.image +'" class="card-img-top" alt="...">\n' +
         '        <div class="card-body">\n' +
         '            <h4 class="card-title">' + this.name + '</h4>\n' +
         '            <span id="jetpack-id" class="invisible">' + this.id + '</span>' +
         '        </div>\n' +
         '        <div class="btn-group" role="group"">' +
-        //'            <button type="button" id="display_jetpack_view_id/'+jetpack.id+'" class="btn btn-outline-primary edit_button_class" data-toggle="modal" data-target="#view_modal"style="">Voir</button>' +
+            (show_book_button ?
+        '            <button type="button" id="diplay_jetpack_booking_id/' + this.id + '"class="btn btn-outline-success booking_button_class" data-toggle="modal" data-target="#booking_modal">Réserver</button>' : (
         '            <button type="button" id="display_jetpack_edit_id/'+this.id+'" class="btn btn-outline-primary edit_button_class dual-btn-equal-width" data-toggle="modal" data-target="#edit_modal"style="">Modifier</button>' +
-        //'            <button type="button" id="diplay_jetpack_booking_id/'+jetpack.id+'"class="btn btn-outline-success booking_button_class" data-toggle="modal" data-target="#booking_modal">Réserver</button>' +
-        '            <button type="button" id="display_jetpack_delete_id/'+this.id+'"class="btn btn-outline-danger delete_button_class dual-btn-equal-width" data-toggle="modal" data-target="#delete_modal">Supprimer</button>' +
+        '            <button type="button" id="display_jetpack_delete_id/'+this.id+'"class="btn btn-outline-danger delete_button_class dual-btn-equal-width" data-toggle="modal" data-target="#delete_modal">Supprimer</button>')) +
         '        </div>' +
         '    </div>' +
         '</div>'
-    }
-
-    toCardAvailableJetPacks(){
-        return '' +
-            '<div class="col-lg-4 col-md-6 mb-4">' +
-            '   <div class="col-lg-4 col-md-6 mb-4">' +
-            '       <div class="card h box zoom" style="width: 18rem;">\n' +
-            '           <img src="' + this.image + '" class="card-img-top" alt="...">\n' +
-            '           <div class="card-body">\n' +
-            '               <h4 class="card-title">' + this.name + '</h4>\n' +
-            '               <span id="jetpack-id" class="hidden">' + this.id + '</span>' +
-            '           </div>\n' +
-            '           <div class="btn-group" role="group"">' +
-            '               <button type="button" id="diplay_jetpack_booking_id/' + this.id + '"class="btn btn-outline-success booking_button_class" data-toggle="modal" data-target="#booking_modal">Réserver</button>' +
-            '           </div>' +
-            '       </div>' +
-            '   </div>' +
-            '</div>'
     }
 };
